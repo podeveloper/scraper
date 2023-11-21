42f3caf8e2f556bbff302afda4b023a8f5e5c455
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-21 16:24:50 +0300

    Custom Livewire Js Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+\Livewire\Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/scraper/livewire/livewire.js', $handle);
+});
